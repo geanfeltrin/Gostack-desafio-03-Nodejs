86d70f57d3fbb394bd15c4d7f285127c11a5e51c
Gean Feltrin <gean_costa@hotmail.com>
2020-05-27 09:45:18 -0300

    Create transactionService

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  type: 'income' | 'outcome';
+  title: string;
+  value: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ type, value, title }: Request): Transaction {
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+      if (balance.total <= 0 || balance.total <= value) {
+        throw Error(
+          'You can not able to create "outcome", because your balance is negative or value is than your total',
+        );
+      }
+    }
+    const transaction = this.transactionsRepository.create({
+      type,
+      value,
+      title,
+    });
+
+    return transaction;
   }
 }
 
